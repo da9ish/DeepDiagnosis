c24783bdbc5750e15e7ba861a71abdb430ff8bc4
Danish Shah <shah.danish.1996@gmail.com>
2017-03-25 22:47:53 +0530

    Added APK

--- Android/app/src/main/java/com/vatos/deepdiagnosis/MainActivity.java
+++ Android/app/src/main/java/com/vatos/deepdiagnosis/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<String> selectable, pic;
     FloatingActionButton fab;
     ProgressBar progressBar;
-    TextView result;
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,40 +49,38 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        result = (TextView) findViewById(R.id.result);
+        text = (TextView) findViewById(R.id.progress_text);
         progressBar = (ProgressBar) findViewById(R.id.progress_bar);
         fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-            if (result.getVisibility() == View.VISIBLE){
-                result.setVisibility(View.GONE);
-            }
-            selectable = new ArrayList<String>();
-            if (Build.VERSION.SDK_INT >= 23){
-                int perm = ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE);
-                if(perm != PackageManager.PERMISSION_GRANTED){
-                    requestPermission();
-                }else{
+
+                selectable = new ArrayList<String>();
+                if (Build.VERSION.SDK_INT >= 23) {
+                    int perm = ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE);
+                    if (perm != PackageManager.PERMISSION_GRANTED) {
+                        requestPermission();
+                    } else {
+                        new FilePickerBuilder().setMaxCount(1)
+                                .setSelectedFiles(selectable)
+                                .setActivityTheme(R.style.AppTheme)
+                                .pickPhoto(MainActivity.this);
+                    }
+                } else {
                     new FilePickerBuilder().setMaxCount(1)
                             .setSelectedFiles(selectable)
                             .setActivityTheme(R.style.AppTheme)
                             .pickPhoto(MainActivity.this);
                 }
-            }else{
-                new FilePickerBuilder().setMaxCount(1)
-                        .setSelectedFiles(selectable)
-                        .setActivityTheme(R.style.AppTheme)
-                        .pickPhoto(MainActivity.this);
-            }
             }
         });
     }
 
-    public void requestPermission(){
-        if(!ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+    public void requestPermission() {
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
-        }else{
+        } else {
             new AlertDialog.Builder(this)
                     .setTitle("Permission Needed")
                     .setMessage("DeepDiagnosis requires permission to access your photos")
@@ -105,9 +103,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
 
-        switch (requestCode){
+        switch (requestCode) {
             case 1:
-                if(grantResults != null && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                if (grantResults != null && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     new FilePickerBuilder().setMaxCount(1)
                             .setSelectedFiles(selectable)
                             .setActivityTheme(R.style.AppTheme)
@@ -119,9 +117,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-        switch (requestCode){
+        switch (requestCode) {
             case FilePickerConst.REQUEST_CODE_PHOTO:
-                if(data != null) {
+                if (data != null) {
                     pic = new ArrayList<>();
                     pic.addAll(data.getStringArrayListExtra(FilePickerConst.KEY_SELECTED_MEDIA));
                     new SendImageForDiagnosis().execute();
@@ -133,153 +131,187 @@ public class MainActivity extends AppCompatActivity {
     class SendImageForDiagnosis extends AsyncTask<Void, Void, Void> {
 
         String response = null;
+
         @Override
         protected void onPreExecute() {
 
             progressBar.setVisibility(View.VISIBLE);
+            text.setVisibility(View.VISIBLE);
+            text.setText("Uploading");
             fab.setVisibility(View.GONE);
         }
 
         @Override
         protected Void doInBackground(Void... params) {
-            System.out.println(pic.get(0));
-            String file_name;
-            String file_dir[] = pic.get(0).split("/");
-            int count = file_dir.length;
-            file_name = file_dir[count - 1];
-            System.out.println(file_name);
+            if (pic.size() > 0) {
+                System.out.println(pic.get(0));
+                String file_name;
+                String file_dir[] = pic.get(0).split("/");
+                int count = file_dir.length;
+                file_name = file_dir[count - 1];
+                System.out.println(file_name);
 
-            final String uploadFilePath = pic.get(0);
-            HttpURLConnection conn = null;
-            DataOutputStream dos = null;
-            String lineEnd = "\r\n";
-            String twoHyphens = "--";
-            String boundary = "*****";
-            int serverResponseCode = 0;
-            int bytesRead, bytesAvailable, bufferSize;
-            byte[] buffer;
-            int maxBufferSize = 4 * 1024 * 1024;
-            File sourceFile = new File(uploadFilePath);
+                final String uploadFilePath = pic.get(0);
+                HttpURLConnection conn = null;
+                DataOutputStream dos = null;
+                String lineEnd = "\r\n";
+                String twoHyphens = "--";
+                String boundary = "*****";
+                int serverResponseCode = 0;
+                int bytesRead, bytesAvailable, bufferSize;
+                byte[] buffer;
+                int maxBufferSize = 4 * 1024 * 1024;
+                File sourceFile = new File(uploadFilePath);
 
-            if (!sourceFile.isFile()) {
+                if (!sourceFile.isFile()) {
 
-                Log.e("uploadFile", "Source File not exist :"
-                        + uploadFilePath + "" + file_name);
+                    Log.e("uploadFile", "Source File not exist :"
+                            + uploadFilePath + "" + file_name);
 
-                runOnUiThread(new Runnable() {
-                    public void run() {
+                    runOnUiThread(new Runnable() {
+                        public void run() {
 
-                    }
-                });
+                        }
+                    });
 
-                return null;
+                    return null;
+                } else {
+                    try {
 
-            } else {
-                try {
+                        // open a URL connection to the Servlet
+                        FileInputStream fileInputStream = new FileInputStream(sourceFile);
+                        URL url = new URL("http://192.168.1.5:8000/upload/");
 
-                    // open a URL connection to the Servlet
-                    FileInputStream fileInputStream = new FileInputStream(sourceFile);
-                    URL url = new URL("http://192.168.137.1:8000/upload/");
+                        // Open a HTTP  connection to  the URL
+                        conn = (HttpURLConnection) url.openConnection();
+                        conn.setDoInput(true); // Allow Inputs
+                        conn.setDoOutput(true); // Allow Outputs
+                        conn.setUseCaches(false); // Don't use a Cached Copy
+                        conn.setRequestMethod("POST");
+                        conn.setRequestProperty("Connection", "Keep-Alive");
+                        conn.setRequestProperty("ENCTYPE", "multipart/form-data");
+                        conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
 
-                    // Open a HTTP  connection to  the URL
-                    conn = (HttpURLConnection) url.openConnection();
-                    conn.setDoInput(true); // Allow Inputs
-                    conn.setDoOutput(true); // Allow Outputs
-                    conn.setUseCaches(false); // Don't use a Cached Copy
-                    conn.setRequestMethod("POST");
-                    conn.setRequestProperty("Connection", "Keep-Alive");
-                    conn.setRequestProperty("ENCTYPE", "multipart/form-data");
-                    conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+                        dos = new DataOutputStream(conn.getOutputStream());
 
-                    dos = new DataOutputStream(conn.getOutputStream());
+                        dos.writeBytes(twoHyphens + boundary + lineEnd);
+                        dos.writeBytes("Content-Disposition: form-data; name=\"file\";filename=\""
+                                + file_name + "\"" + lineEnd);
 
-                    dos.writeBytes(twoHyphens + boundary + lineEnd);
-                    dos.writeBytes("Content-Disposition: form-data; name=\"file\";filename=\""
-                            + file_name + "\"" + lineEnd);
+                        dos.writeBytes(lineEnd);
 
-                    dos.writeBytes(lineEnd);
-
-                    // create a buffer of  maximum size
-                    bytesAvailable = fileInputStream.available();
-
-                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
-                    buffer = new byte[bufferSize];
-
-                    // read file and write it into form...
-                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-
-                    while (bytesRead > 0) {
-
-                        dos.write(buffer, 0, bufferSize);
+                        // create a buffer of  maximum size
                         bytesAvailable = fileInputStream.available();
+
                         bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                        buffer = new byte[bufferSize];
+
+                        // read file and write it into form...
                         bytesRead = fileInputStream.read(buffer, 0, bufferSize);
 
-                    }
+                        while (bytesRead > 0) {
 
-                    // send multipart form data necesssary after file data...
-                    dos.writeBytes(lineEnd);
-                    dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+                            dos.write(buffer, 0, bufferSize);
+                            bytesAvailable = fileInputStream.available();
+                            bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                            bytesRead = fileInputStream.read(buffer, 0, bufferSize);
 
-                    // Responses from the server (code and message)
-                    serverResponseCode = conn.getResponseCode();
-                    String serverResponseMessage = conn.getResponseMessage();
+                        }
 
-                    BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                    System.out.println(br.readLine());
+                        // send multipart form data necesssary after file data...
+                        dos.writeBytes(lineEnd);
+                        dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
 
+                        if (serverResponseCode == 200) {
+                            runOnUiThread(new Runnable() {
+                                public void run() {
+                                text.setText("Processing");
+                                }
 
-                    Log.i("uploadFile", "HTTP Response is : "
-                            + serverResponseMessage + ": " + serverResponseCode);
+                            });
+                        }
+                        // Responses from the server (code and message)
+                        serverResponseCode = conn.getResponseCode();
+                        String serverResponseMessage = conn.getResponseMessage();
 
-                    response = br.readLine();
-                    if (serverResponseCode == 200) {
+                        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                        response = br.readLine();
+
+                        Log.i("uploadFile", "HTTP Response is : "
+                                + serverResponseMessage + ": " + serverResponseCode);
+
+                        //close the streams //
+                        fileInputStream.close();
+                        dos.flush();
+                        dos.close();
+
+                    } catch (MalformedURLException ex) {
+
+                        ex.printStackTrace();
+
                         runOnUiThread(new Runnable() {
                             public void run() {
-                                Toast.makeText(MainActivity.this, "File Upload Complete", Toast.LENGTH_LONG).show();
+                                Toast.makeText(MainActivity.this, "MalformedURLException",
+                                        Toast.LENGTH_SHORT).show();
                             }
+                        });
 
+                    } catch (Exception e) {
+
+                        e.printStackTrace();
+
+                        runOnUiThread(new Runnable() {
+                            public void run() {
+                                Toast.makeText(MainActivity.this, "Got Exception : see logcat ",
+                                        Toast.LENGTH_SHORT).show();
+                            }
                         });
                     }
-
-                    //close the streams //
-                    fileInputStream.close();
-                    dos.flush();
-                    dos.close();
-
-                } catch (MalformedURLException ex) {
-
-                    ex.printStackTrace();
-
-                    runOnUiThread(new Runnable() {
-                        public void run() {
-                            Toast.makeText(MainActivity.this, "MalformedURLException",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    });
-
-                } catch (Exception e) {
-
-                    e.printStackTrace();
-
-                    runOnUiThread(new Runnable() {
-                        public void run() {
-                            Toast.makeText(MainActivity.this, "Got Exception : see logcat ",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    });
                 }
-
-                return null;
             }
+            return null;
         }
 
         @Override
         protected void onPostExecute(Void aVoid) {
-            result.setVisibility(View.VISIBLE);
-            result.setText(response);
-
-            progressBar.setVisibility(View.GONE);
+            if (response != null) {
+                String arr[] = response.split(" ");
+                float per = Float.parseFloat(arr[1]) * 100;
+                String type = null;
+                if (arr[0].equals("malignant")) {
+                    type = "Cancerous(Malignant)";
+                } else {
+                    type = "Non-Cancerous(Benine)";
+                }
+                String main = "Category: " + type + " \nConfidence: " + per + "%";
+                System.out.println(main);
+                if (arr[0].equals("malignant")) {
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setTitle("Result")
+                            .setMessage(main)
+                            .setPositiveButton("Get Tips!", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                    new AlertDialog.Builder(MainActivity.this)
+                                            .setTitle("Tips!")
+                                            .setMessage(R.string.malignant)
+                                            .setNegativeButton("Close", null)
+                                            .show();
+                                }
+                            })
+                            .setNegativeButton("Cancel", null)
+                            .show();
+                } else {
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setTitle("Result")
+                            .setMessage(main)
+                            .setNegativeButton("Cancel", null)
+                            .show();
+                }
+                text.setVisibility(View.GONE);
+                progressBar.setVisibility(View.GONE);
+            }
             fab.setVisibility(View.VISIBLE);
         }
     }
